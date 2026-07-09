758ba2615dd415947cb57b8b78c0d6b217c8d457
chad red <chadredrenos24@gmail.com>
2026-07-09 10:46:22 +0800

    Add problem-1-bulalaque.py

---
+++ problem-1-bulalaque.py
@@ -0,0 +1,16 @@
+name = "Matt Joshua"
+age = 16
+gpa = 3.8
+enrolled = True
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(gpa)
+print(type(gpa))
+
+print(enrolled)
+print(type(enrolled)) 
