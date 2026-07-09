4580fcf12d4a0fba72ba560ed8c55f07e7091718
reneejandipositario-wq <reneejandipositario@gmail.com>
2026-07-09 14:36:48 +0800

    Create problem-3-dipositario.py

---
+++ problem-3-dipositario.py
@@ -0,0 +1,10 @@
+age = 17
+
+# This causes a TypeError
+# print("I am " + age + " years old")
+
+# Fixed using str()
+print("I am " + str(age) + " years old")
+
+# Using an f-string
+print(f"I am {age} years old")
