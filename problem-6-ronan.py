f836e1d6499fdf4e9b3e284ae617d78402bef22a
jaredronan65-web <jaredronan65@gmail.com>
2026-07-09 13:53:06 +0800

    Update problem-6-ronan.py

--- problem-6-ronan.py
+++ problem-6-ronan.py
@@ -1,6 +1,7 @@
+
 try:
     age_input = input("Enter your age: ")
     age_next_year = int(age_input) + 1
-    print(f"Next year you will be {age_next_year}.")
+    print(f"Next year you will be {age_next_year}")
 except ValueError:
     print("Please enter a valid number.")
